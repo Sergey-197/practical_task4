ca7b633652ae3347278093e60b38a9add0a5b154
Sergey <spumen35@gmail.com>
2024-04-11 11:29:34 +0300

    .\Book.java

---
+++ Book.java
@@ -0,0 +1,19 @@
+
+public class Book<T> {
+    private T title;
+
+    public Book(T title) {
+        this.title = title;
+    }
+
+    public T getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title=" + title +
+                '}';
+    }
+}
